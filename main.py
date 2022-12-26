04fdb3ebcc91dc18efcc80fdf914ac6f74adeabc
lcub <lcubrilo01@gmail.com>
2022-12-26 23:44:15 +0100

    Deleting columns works

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QListWidgetItem, QTreeWidgetItem, QTreeWidget
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QListWidgetItem, QTreeWidgetItem, QTreeWidget, QWidget, QCheckBox, QHBoxLayout, QMessageBox
 from PyQt5.QtCore import Qt
 
 from PandasModelClass import PandasModel
@@ -13,8 +13,8 @@ class MyApplicationMainWindow(QMainWindow):
         # Slot-signal connections
         self.browseButton.clicked.connect(self.browseFiles)
         self.plotButton.clicked.connect(self.plotData)  
+        self.deleteButton.clicked.connect(self.deleteColumns)
         
-
     def updateFilesToPlot(self, item, column):
         if item.parent() != None: return
         print("UPDATE FILES TO PLOT TRIGGERED")
@@ -82,10 +82,10 @@ class MyApplicationMainWindow(QMainWindow):
         #self.treeWidget.insertTopLevelItems(0, items)
             
             for table in self.paket.tellMeTablesInFile(file):
-                    tmpChild = QTreeWidgetItem(tmp, [table])#(tmp)
-                    #tmpChild.setText(table)
-                    tmpChild.setCheckState(0, Qt.Checked)
-                    #tmp.addChild(tmpChild)
+                tmpChild = QTreeWidgetItem(tmp, [table])#(tmp)
+                #tmpChild.setText(table)
+                tmpChild.setCheckState(0, Qt.Checked)
+                #tmp.addChild(tmpChild)
             
 
         self.treeWidget.itemClicked.connect(self.showData)
@@ -99,33 +99,18 @@ class MyApplicationMainWindow(QMainWindow):
             self.tablesToPlot[file] = set()
             for table in self.paket.tellMeTablesInFile(file):
                 self.tablesToPlot[file].add(table)
-    
-        """def showTables(self, item, column):
-        #self.tableListWidget.clear()
-
-        currentItem = item
-        if currentItem == None: return
-
-        fileName = currentItem.text(column)
-        for table in self.paket.tellMeTablesInFile(fileName):
-            tmp = QListWidgetItem(table)
-            if table in self.tablesToPlot[fileName]:
-                tmp.setCheckState(Qt.Checked)
-            else:
-                tmp.setCheckState(Qt.Unchecked)
-            self.tableListWidget.addItem(tmp)
-
-        self.tableListWidget.itemClicked.connect(self.showData)
-        self.tableListWidget.itemChanged.connect(self.updateTablesToPlot)"""
         
         self.plotButton.enabled = True
-
+        self.exportButton.enabled = True
+        
     def showData(self, item, column):
         # TODO clear prevous??
         #currentItem = self.treeWidget.currentItem()
         if item.parent() == None: return
 
-        dataFrame = self.paket.data[item.parent().text(0)][item.text(0)]
+        self.currentFileSelected = item.parent().text(0)
+        self.currentTableSelected = item.text(0)
+        dataFrame = self.paket.data[self.currentFileSelected][self.currentTableSelected]
 
         self.model = PandasModel(dataFrame)
         self.tableView.setModel(self.model)
@@ -134,8 +119,16 @@ class MyApplicationMainWindow(QMainWindow):
             for val in self.model._dataframe.columns.values:
                 self.xAxisCombo.addItem(val)
                 self.yAxisCombo.addItem(val)
+
+        if len(self.scrollArea.findChildren(QWidget)) == 6:
+            widget = QWidget()
+            layout = QHBoxLayout(widget)
+            for val in self.model._dataframe.columns.values:
+                tmpCheckBox = QCheckBox(val, self.scrollArea)
+                tmpCheckBox.setCheckState(Qt.Checked)
+                layout.addWidget(tmpCheckBox)
+            self.scrollArea.setWidget(widget)
             
-    
     def plotData(self):
         x_axis = self.xAxisCombo.currentText()
         y_axis = self.yAxisCombo.currentText()
@@ -145,7 +138,36 @@ class MyApplicationMainWindow(QMainWindow):
             return
             
         self.paket.plotData(x_axis, y_axis, self.filesToPlot, self.tablesToPlot)
+    
+    def deleteColumns(self):
+        self.tableView.setModel(None)
+        columnsToDelete = []
+
+        for checkBox in self.scrollArea.findChildren(QCheckBox):
+            if checkBox.checkState() == Qt.Unchecked:
+                if checkBox.isEnabled():
+                    columnName = checkBox.text()
+                    columnsToDelete.append(columnName)
+
+                    index = self.xAxisCombo.findText(columnName)
+                    self.xAxisCombo.removeItem(index)
+                    
+                    index = self.yAxisCombo.findText(columnName)
+                    self.yAxisCombo.removeItem(index)
+
+                    checkBox.setEnabled(False)
         
+        for file in self.paket.tellMeFiles():
+            for table in self.paket.tellMeTablesInFile(file):
+
+                for columnToDel in columnsToDelete:
+                    del self.paket.data[file][table][columnToDel]
+
+        msg = QMessageBox()
+
+        msg.setWindowTitle("Notification")
+        msg.setText("You have just deleted the columns that were unselected. To get them back, reload the files.")
+        x = msg.exec_()  
 
 app = QtWidgets.QApplication(sys.argv)
 window = MyApplicationMainWindow()
