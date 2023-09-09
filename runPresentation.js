6600ff5f43fbe686d015acb5e9568e5a6c90fea9
lcub <lcubrilo01@gmail.com>
2023-09-09 05:32:09 +0200

    fix

--- runPresentation.js
+++ runPresentation.js
@@ -2,12 +2,19 @@ define([
     'base/js/namespace',
     'base/js/events'
 ], function(Jupyter, events) {
-    console.log("runPresentation.js loaded");  // Debug statement
+    console.log("runPresentation.js loaded");
 
     function startSlideshow() {
-        Jupyter.actions.call('rise:slideshow');
+        if (Jupyter && Jupyter.actions) {
+            console.log("Attempting to start RISE slideshow");
+            Jupyter.actions.call('rise:slideshow');
+        } else {
+            console.log("Jupyter actions not ready");
+        }
     }
 
+    // Listen for multiple events
     events.on("kernel_ready.Kernel", startSlideshow);
     events.on("notebook_loaded.Notebook", startSlideshow);
+    events.on("rise:loaded", startSlideshow);
 });
